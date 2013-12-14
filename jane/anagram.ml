0781c559f83ee380a2471c52484d26302ae2a2f3
Antti Halme <antti.halme@iki.fi>
2013-12-14 15:53:54 +0200

    Add QC tests for anagrams

--- jane/anagram.ml
+++ jane/anagram.ml
@@ -27,9 +27,13 @@ let charRemover s arr =
   counts
 ;;
 
-let isAnagram a b = 
+let isAnagram a b p = 
   let counts = charRemover b (charCounts a) in  
-  Batteries.Array.iter (fun x -> printf "%d" x) counts; print_newline ();
+  if p then begin 
+    Batteries.Array.iter (fun x -> printf "%d" x) counts; print_newline ()
+  end
+  else ();
+  
   let errs = Batteries.Array.fold_left 
     (fun s x -> s + (if x = 0 then 0 else 1)) 0 counts in  
   errs = 0
@@ -42,12 +46,43 @@ let () =
   and str2 = "hello dorld"
   and str3 = "wello horld" in
   let tryAnagrams s t =
-    let res = isAnagram s t in
+    let res = isAnagram s t true in
     printf " '%s' %ca= '%s'\n" s (if res then '=' else '!') t;
   in
 
   tryAnagrams str1 str2;
-  tryAnagrams str1 str3
+  tryAnagrams str1 str3;
+
+  Random.init (Int.of_float (Time.to_float (Time.now ()))); (* seedin' *)
+
+  let alphabet = Batteries.String.concat "" alpha in
+  let asciiGen dict = (fun () -> dict.[Random.int (String.length dict)]) in
+  let lengthGen max = (fun () -> Random.int max) in
+  let maxlen = 20 in
+  let stringGen = QC.sg ~char_gen:(asciiGen alphabet) ~size_gen:(lengthGen maxlen) in 
+
+  printf "\n Generating some randy chars: ";
+  QC.repeat 10 (printf "%c ") (asciiGen alphabet);
+  printf "\n";
+
+  printf "\n Generating some randy strings:\n";
+  let shuf s = BatString.of_list (BatArray.to_list (Batteries.Random.shuffle (BatString.enum s))) in
+  QC.repeat 10 (fun s -> printf "   %20s -- %s\n" s (shuf s)) stringGen;
+  printf "\n";
+
+  printf "Trying out laws.\n";
+  let law_try law should_fail f =
+    begin
+      let on_ok = (if should_fail then "(bad)" else "(ok)") in
+      let on_fail = (if should_fail then "(ok)" else "(bad)") in      
+      try 
+	QC.laws_exn law 10 stringGen f;
+	printf "law %s passed %s\n" law on_ok;
+      with Failure s -> print_string s; printf " %s\n" on_fail;
+    end
+  in
+  law_try "shuffle" false (fun a -> isAnagram a (shuf a) false);
+  law_try "doubling" true (fun a -> isAnagram a (BatString.repeat a 2) false);
 
 ;;
   
